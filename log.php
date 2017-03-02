34713046438a0f598ebfb4788c2580c369ea6ae7
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-02 20:35:01 +0800

    log add index

--- log.php
+++ log.php
@@ -1,11 +1,15 @@
 <?php
 function WriteLog($message="") {
 	global $C, $G;
+	if (!isset($G["logcnt"])) {
+		$G["logcnt"] = 0;
+	}
+	$G["logcnt"] ++;
 	$time = date("Y-m-d H:i:s");
 	$hash = md5(json_encode(array("time"=>$time, "message"=>$message)));
 	$sth = $G["db"]->prepare("INSERT INTO `{$C['DBTBprefix']}log` (`time`, `message`, `hash`) VALUES (:time, :message, :hash)");
 	$sth->bindValue(":time", $time);
-	$sth->bindValue(":message", $message);
+	$sth->bindValue(":message", "[".$G["logcnt"]."] ".$message);
 	$sth->bindValue(":hash", $hash);
 	$sth->execute();
 }
