0f62e86f9a69451609bb92a8c01fed6ef7481617
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-02 19:58:58 +0800

    config format

--- config/config.example.php
+++ config/config.example.php
@@ -19,9 +19,17 @@ $C['fetch'] = 'http://www.tnfsh.tn.edu.tw/files/501-1000-1012-1.php';
 $G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8', $C["DBuser"], $C["DBpass"]);
 
 $M["nottext"] = "僅接受文字訊息";
-$M["notcommand"] = "本粉專由機器人自動運作\n啟用訊息通知請輸入 /start\n顯示所有命令輸入 /help";
+$M["notcommand"] = "本粉專由機器人自動運作\n".
+	"啟用訊息通知請輸入 /start\n".
+	"顯示所有命令輸入 /help";
 $M["start"] = "已啟用訊息通知";
 $M["stop"] = "已停用訊息通知";
-$M["help"] = "可用命令\n/start 啟用訊息通知\n/stop 停用訊息通知\n/help 顯示所有命令";
+$M["help"] = "可用命令\n".
+	"/start 啟用訊息通知\n".
+	"/stop 停用訊息通知\n".
+	"/last 顯示最後一筆通知\n".
+	"/last N 顯示最後N筆通知\n".
+	"/help 顯示所有命令";
 $M["fail"] = "指令失敗";
-$M["wrongcommand"] = "無法辨識命令\n輸入 /help 取得可用命令";
+$M["wrongcommand"] = "無法辨識命令\n".
+	"輸入 /help 取得可用命令";
