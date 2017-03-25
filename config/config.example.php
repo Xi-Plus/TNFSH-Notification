6c0e412d376f2c1e0bc26780431da6c275874f15
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-25 15:10:27 +0800

    autostop

--- config/config.example.php
+++ config/config.example.php
@@ -14,6 +14,9 @@ $C['DBuser'] = 'user';
 $C['DBpass'] = 'pass';
 $C['DBTBprefix'] = 'tnfsh_notification_';
 
+$C['UnreadLimit'] = 86400*7;
+$C['UnreadLimitText'] = "7天";
+
 $C['fetch'] = 'http://www.tnfsh.tn.edu.tw/files/501-1000-1012-1.php';
 
 $C["allowsapi"] = array("cli");
